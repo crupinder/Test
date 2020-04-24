8fe4c193dbf6b4c1dec7af0fd0dfa3e0ffd00e57
crupinder <53912073+crupinder@users.noreply.github.com>
2020-04-24 00:13:42 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,12 +1,3 @@
-package Test;
-
-import java.io.*;
-import java.util.*;
-import java.util.function.UnaryOperator;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
